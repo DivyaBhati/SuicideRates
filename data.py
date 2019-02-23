2ca83f7111cdb241c3964d39ceddebc7b5f7776c
Divya <dipsbhati6@gmail.com>
2019-02-23 16:52:38 -0600

    First file

---
+++ data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+data = pd.read_csv("who_suicide_statistics.csv")
+usa = data.loc[data['country'] == "United States of America"]
+
+def age_group_data(country):
+    ages = {"75+ years": {}, "15-24 years":{}, "25-34 years": {}, "35-54 years":{}, "5-14 years":{}, "55-74 years":{}}
+    for i in range(1979, 2016):
+        year_data = country.loc[country['year'] == i]
+        for index, row in year_data.iterrows():
+            val = row['suicides_no'] / row['population'] * 100000
+            ages[row['age']][i] = round(ages[row['age']].get(i, 0) + val, 2)
+    return ages
+print(age_group_data(usa))
