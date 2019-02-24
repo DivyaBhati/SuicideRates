04b5246b6d87f19f4363170858b97527552235dc
Daniel <danielrferriss@gmail.com>
2019-02-23 23:27:16 -0600

    tracks overall suicide rate now

--- data.py
+++ data.py
@@ -9,18 +9,17 @@ gdp = pd.read_csv("data/gdp/gdp.csv")
 #Returns total suicides by age group for a country
 def ageGroupData(country):
     data = suicides.loc[suicides['country'] == country]
-    ages = {"5-14 years":{}, "15-24 years":{}, "25-34 years": {}, "35-54 years":{}, "55-74 years":{}, "75+ years": {}}
+    ages = {"5-14 years":{}, "15-24 years":{}, "25-34 years": {}, "35-54 years":{}, "55-74 years":{}, "75+ years": {}, "Overall": {}}
     for i in range(1979, 2016):
-    	total_pop = 0
-    	total_suicides
+        total_pop = 0
+        total_suicides = 0
         year_data = data.loc[data['year'] == i]
         for index, row in year_data.iterrows():
             val = row['suicides_no'] / row['population'] * 100000
-            total_suicides += row['suicides_no']
-            total_pop += row['population']
+            total_suicides = total_suicides + row['suicides_no']
+            total_pop = total_pop + row['population']
             ages[row['age']][i] = round(ages[row['age']].get(i, 0) + val, 2)
-        
-
+        ages['Overall'][i] = round(total_suicides / total_pop * 100000, 2)
     return ages
 
 #Returns data in list format for graphing
@@ -63,7 +62,7 @@ def something_and_suicides(data, suicide):
 	years = range(start_year, end_year)
 
 
-def intepolate_list(start_year, end_year, data)
+def interpolate_list(start_year, end_year, data):
 	datalist = []
 	for i in range(start_year, end_year):
 		if i in data_years:
@@ -74,16 +73,3 @@ def intepolate_list(start_year, end_year, data)
 	datalist = pd.Series(datalist).interpolate().tolist()
 	return(datalist)
 
-
-
-
-
-
-
-
-
-
-
-
-print(ageGroupData('United States of America'))
-# print(gdp_data_country('United States of America'))
