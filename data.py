a0b23684d71d67e453aab31eee519e524e2b1b62
Divya <dipsbhati6@gmail.com>
2019-02-23 19:29:52 -0600

    New changes

--- data.py
+++ data.py
@@ -5,12 +5,30 @@ from collections import defaultdict
 data = pd.read_csv("who_suicide_statistics.csv")
 usa = data.loc[data['country'] == "United States of America"]
 
-def age_group_data(country):
-    ages = {"75+ years": {}, "15-24 years":{}, "25-34 years": {}, "35-54 years":{}, "5-14 years":{}, "55-74 years":{}}
+#Returns total suicides by age group for a country
+def ageGroupData(country):
+    ages = {"5-14 years":{}, "15-24 years":{}, "25-34 years": {}, "35-54 years":{}, "55-74 years":{}, "75+ years": {}}
     for i in range(1979, 2016):
         year_data = country.loc[country['year'] == i]
         for index, row in year_data.iterrows():
             val = row['suicides_no'] / row['population'] * 100000
             ages[row['age']][i] = round(ages[row['age']].get(i, 0) + val, 2)
     return ages
-print(age_group_data(usa))
+
+#Returns data in list format for graphing
+def dataForGraphing(country):
+    ages = ageGroupData(country)
+    lists = []
+    for group in ages:
+        curr = []
+        for key, val in ages[group].items():
+            curr.append(val)
+        lists.append(curr)
+    return lists
+
+years = ""
+for i in range(1979, 2016):
+    years += "\'"+str(i)+"\', "
+print(years)
+
+print(dataForGraphing(usa))
