ec2dbf132e211244eb7b63a8847694a053f05ca9
Daniel <danielrferriss@gmail.com>
2019-02-23 19:28:19 -0600

    ADDED SOME MORE METHODS FOR GDP

--- data.py
+++ data.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
-data = pd.read_csv("who_suicide_statistics.csv")
-usa = data.loc[data['country'] == "United States of America"]
+suicides = pd.read_csv("data/who_suicide_statistics.csv")
+gdp = pd.read_csv("data/gdp/gdp.csv")
+
 
 def age_group_data(country):
+    country = suicides.loc[suicides['country'] == country]
     ages = {"75+ years": {}, "15-24 years":{}, "25-34 years": {}, "35-54 years":{}, "5-14 years":{}, "55-74 years":{}}
     for i in range(1979, 2016):
         year_data = country.loc[country['year'] == i]
@@ -13,4 +15,16 @@ def age_group_data(country):
             val = row['suicides_no'] / row['population'] * 100000
             ages[row['age']][i] = round(ages[row['age']].get(i, 0) + val, 2)
     return ages
-print(age_group_data(usa))
+
+def gdp_data():
+	dict_list = gdp.to_dict(orient='records')
+	gdp_dict_country = {}
+	for i in range(len(dict_list)):
+		gdp_dict_country[dict_list[i]['Country Name']] = dict_list[i]
+	return(gdp_dict_country)
+	
+def gdp_data_country(country):
+	return(gdp_data()[country])
+
+
+
