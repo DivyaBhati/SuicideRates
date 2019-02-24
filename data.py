44035fedd6ea4ffd1a6d3960cbe6175816bb8840
Daniel <danielrferriss@gmail.com>
2019-02-24 03:06:23 -0600

    commented and cleaned

--- data.py
+++ data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-#Returns total suicides by age group for a country
+#Returns total suicides by age group for a inputted country
+#return a dictionary (age ranges) of dictionaries (years:suicide rate)
+#suicide rate is number out of 100,000 of the population for that age subset
 def age_group_data(country):
     suicides = pd.read_csv("data/who_suicide_statistics.csv")
     data = suicides.loc[suicides['country'] == country]
@@ -18,6 +20,9 @@ def age_group_data(country):
         ages['Overall'][i] = round(total_suicides / total_pop * 100000, 2)
     return ages
 
+#rawdata is a csv with the first column country name and remaining years
+#works with most WHO data as long as you delete their weird erroneous empty columns and rows they put in 
+#return is a dictionary of the form year:value for the inputted country and data
 def who_extract(rawdata, country):
 	dict_list = rawdata.to_dict(orient='records')
 	dict_country = {}
@@ -28,29 +33,10 @@ def who_extract(rawdata, country):
 	data = {int(k):round(v, 3) for k,v in data.items()}
 	return(data)
 
-def gdp_data(country):
-	data = pd.read_csv("data/gdp.csv")
-	return(who_extract(data, country))
-
-
-def pollution_data(country):
-	data = pd.read_csv("data/pollution.csv")
-	return(who_extract(data, country))
-
-
-def education_data(country):
-	data = pd.read_csv('data/education.csv')
-	return(who_extract(data, country))
-
-def internet_data(country):
-	data = pd.read_csv('data/internetusers.csv')
-	return(who_extract(data, country))
-
-def alcohol_data(country):
-	data = pd.read_csv('data/alcohol.csv')
-	return(who_extract(data, country))
-
-
+#data is a dictionary of the form year:value
+#suicides the return from age_group_data
+#returns a list of lists that are all for the same years
+#form is [[adjusted input data], [years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]
 def something_and_suicides(country, data):
 	suicides = age_group_data(country)
 	data_years = data.keys()
@@ -64,6 +50,8 @@ def something_and_suicides(country, data):
 	something = interpolate_dict(start_year, end_year, data)
 	return([something] + suicides_list_adjusted(suicides, start_year, end_year))
 
+#suicide is return from age_group_data
+#returns tuple of list of years and list of dictionaries of lists of the form age_range:list of suicide rates by year
 def suicides_list_adjusted(suicide, start_year=0, end_year=0):
 	if start_year == 0:
 		suicide_years = suicide['5-14 years'].keys()
@@ -75,10 +63,13 @@ def suicides_list_adjusted(suicide, start_year=0, end_year=0):
 		suicides_list.append(interpolate_dict(start_year, end_year, suicide[key]))
 	return([years] + suicides_list)
 
+#returns suicides in this form [[years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]]
 def country_suicides(country):
 	suicides = age_group_data(country)
 	return(suicides_list_adjusted(suicides))
 
+#fills in any missing data between given years
+#return is a list
 def interpolate_dict(start_year, end_year, data):
 	datalist = []
 	data_years = data.keys()
@@ -90,19 +81,32 @@ def interpolate_dict(start_year, end_year, data):
 	datalist = pd.Series(datalist).interpolate(method='linear').tolist()
 	return(datalist)
 
+#gets list of lists of this form (all data indexed by the same year range):
+#[[gdp], [years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]
 def gdp_and_suicides(country):
-	return(something_and_suicides(country, gdp_data(country)))
+	data = pd.read_csv("data/gdp.csv")
+	return(something_and_suicides(country, who_extract(data, country)))
 
+#gets list of lists of this form (all data indexed by the same year range):
+#[[pollution], [years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]
 def pollution_and_suicides(country):
-	return(something_and_suicides(country, pollution_data(country)))
+	data = pd.read_csv("data/pollution.csv")
+	return(something_and_suicides(country, who_extract(data, country)))
 
+#gets list of lists of this form (all data indexed by the same year range):
+#[[education index], [years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]
 def education_and_suicides(country):
-	return(something_and_suicides(country, education_data(country)))
+	data = pd.read_csv('data/education.csv')
+	return(something_and_suicides(country, who_extract(data, country)))
 
+#gets list of lists of this form (all data indexed by the same year range):
+#[[internet usage rate], [years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]
 def internet_and_suicides(country):
-	return(something_and_suicides(country, internet_data(country)))
+	data = pd.read_csv('data/internetusers.csv')
+	return(something_and_suicides(country, who_extract(data, country)))
 
+#gets list of lists of this form (all data indexed by the same year range):
+#[[alcohol consumption], [years], [5-14 years], [15-24 years], [25-34 years], [35-54 years], [55-74 years], [75+ years], [Overall]]
 def alcohol_and_suicides(country):
-	return(something_and_suicides(country, alcohol_data(country)))
-
-print(alcohol_and_suicides('United States of America'))
+	data = pd.read_csv('data/alcohol.csv')
+	return(something_and_suicides(country, who_extract(data, country)))
