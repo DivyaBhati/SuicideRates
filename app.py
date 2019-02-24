dcd29593a8042e52e2d2ff1cb69c8e20e8858552
Divya <dipsbhati6@gmail.com>
2019-02-24 00:04:31 -0600

    Made it flask

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import Required, AnyOf
+from flask_navigation import Navigation
+import pandas as pd
+import math
+from data import *
+
+
+
+app = Flask(__name__)
+nav = Navigation(app)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'reallyreallyreallysecretkey'
+
+class compare_form(FlaskForm):
+    country = StringField(u'Country: ', validators=[Required()])
+    factor = StringField(u'Factor: ', validators=[Required()])
+    submit = SubmitField(u'Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return render_template('index.html')
+
+@app.route('/compare', methods=['GET', 'POST'])
+def compare():
+    country = None
+    factor = None
+    form = compare_form()
+    if form.validate_on_submit():
+        country = form.country
+        factor = form.factor
+        return render_template('compare.html', form=form, country=country, factor=factor)
+    return render_template('compare.html', form=form, country=country, factor=factor)
+
+
+#Run app
+if __name__ == '__main__':
+    app.run()
